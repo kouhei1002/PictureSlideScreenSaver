3867eb80da3b56034bfe748f601fd6e70279a475
kouhei1002 <kouhei.4434@gmail.com>
2015-05-01 17:56:09 +0900

    Revert "Revert "first commit!""
    This reverts commit 36ac474ff84693cebbcca57ede95d04cc6fd839c.

---
+++ ScreenSaverPictures/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ScreenSavaverPictures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.showPictureTimer = new System.Windows.Forms.Timer(this.components);
+            this.WaitPictureTimer = new System.Windows.Forms.Timer(this.components);
+            this.HiddePictureTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // showPictureTimer
+            // 
+            this.showPictureTimer.Interval = 16;
+            this.showPictureTimer.Tick += new System.EventHandler(this.showPictureTimer_Tick);
+            // 
+            // WaitPictureTimer
+            // 
+            this.WaitPictureTimer.Interval = 3000;
+            this.WaitPictureTimer.Tick += new System.EventHandler(this.WaitPictureTimer_Tick);
+            // 
+            // HiddePictureTimer
+            // 
+            this.HiddePictureTimer.Interval = 16;
+            this.HiddePictureTimer.Tick += new System.EventHandler(this.HiddePictureTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1198, 603);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer showPictureTimer;
+        private System.Windows.Forms.Timer WaitPictureTimer;
+        private System.Windows.Forms.Timer HiddePictureTimer;
+
+
+    }
+}
+
---
+++ ScreenSaverPictures/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace ScreenSavaverPictures
+{
+    public partial class Form1 : Form
+    {
+        // Test Program
+        List<String> fileNameList;
+        Queue<String> fileNameQueue;
+
+        String dir;
+
+        PicturePanel currentPicturePanel;
+        PicturePanel nextPicturePanel;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            this.BackColor = Color.Black;
+            this.dir = @"C:\Users\kouhei\Dropbox\Pictures\Illust";
+            this.fileNameList = new List<String>();
+
+            this.SetStyle(ControlStyles.DoubleBuffer, true);
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // load picuture filename.
+            initFileNameList();
+            initFileNameQueue();
+
+            // set first picture panel.
+            this.currentPicturePanel = this.newPicturePanel();
+            this.currentPicturePanel.Size = new System.Drawing.Size(this.Width, this.Height);
+            this.Controls.Add(this.currentPicturePanel);
+
+            Debug.WriteLine("Start Screen Saver!");
+
+            this.showPictureTimer.Start();
+        }
+
+        private void showPictureTimer_Tick(object sender, EventArgs e)
+        {
+            if (this.currentPicturePanel.showPicture())
+            {
+                this.showPictureTimer.Stop();
+                this.nextPicturePanel = this.newPicturePanel();
+                this.nextPicturePanel.Size = new System.Drawing.Size(this.Width, this.Height);
+                this.WaitPictureTimer.Start();
+            }
+        }
+
+        private void WaitPictureTimer_Tick(object sender, EventArgs e)
+        {
+            this.WaitPictureTimer.Stop();
+            this.HiddePictureTimer.Start();
+        }
+
+        private void HiddePictureTimer_Tick(object sender, EventArgs e)
+        {
+            if (this.currentPicturePanel.hiddePicture())
+            {
+                this.HiddePictureTimer.Stop();
+
+                this.Controls.Remove(this.currentPicturePanel);
+                this.currentPicturePanel.Dispose();
+                this.currentPicturePanel = this.nextPicturePanel;
+                this.Controls.Add(this.currentPicturePanel);
+
+                this.showPictureTimer.Start();
+            }
+        }
+
+        private PicturePanel newPicturePanel()
+        {
+            int number;
+            int max;
+            List<Image> img = new List<Image>();
+            
+            max = 6;
+            Random cRandom = new System.Random();
+
+            number = cRandom.Next(max);
+
+            switch (number)
+            {
+                case 0:
+                    {
+                        String name1 = this.fileNameQueue.Dequeue();
+                        img.Add(Image.FromFile(name1));
+                        Debug.WriteLine("Case: " + number);
+                        Debug.WriteLine("  Picture1 -> " + name1);
+                        return new PicturePanelA1(this.Width, this.Height, img.ToArray());
+                    }
+                case 1:
+                    {
+                        String name1 = this.fileNameQueue.Dequeue();
+                        String name2 = this.fileNameQueue.Dequeue();
+                        img.Add(Image.FromFile(name1));
+                        img.Add(Image.FromFile(name2));
+                        Debug.WriteLine("Case: " + number);
+                        Debug.WriteLine("  Picture1 -> " + name1);
+                        Debug.WriteLine("  Picture2 -> " + name2);
+                        return new PicturePanelB1(this.Width, this.Height, img.ToArray());
+                    }
+                case 2:
+                    {
+                        String name1 = this.fileNameQueue.Dequeue();
+                        String name2 = this.fileNameQueue.Dequeue();
+                        String name3 = this.fileNameQueue.Dequeue();
+                        img.Add(Image.FromFile(name1));
+                        img.Add(Image.FromFile(name2));
+                        img.Add(Image.FromFile(name3));
+                        Debug.WriteLine("Case: " + number);
+                        Debug.WriteLine("  Picture1 -> " + name1);
+                        Debug.WriteLine("  Picture2 -> " + name2);
+                        Debug.WriteLine("  Picture3 -> " + name3);
+                        return new PicturePanelC1(this.Width, this.Height, img.ToArray());
+                    }
+                case 4:
+                    {
+                        String name1 = this.fileNameQueue.Dequeue();
+                        String name2 = this.fileNameQueue.Dequeue();
+                        String name3 = this.fileNameQueue.Dequeue();
+                        img.Add(Image.FromFile(name1));
+                        img.Add(Image.FromFile(name2));
+                        img.Add(Image.FromFile(name3));
+                        Debug.WriteLine("Case: " + number);
+                        Debug.WriteLine("  Picture1 -> " + name1);
+                        Debug.WriteLine("  Picture2 -> " + name2);
+                        Debug.WriteLine("  Picture3 -> " + name3);
+                        return new PicturePanelD1(this.Width, this.Height, img.ToArray());
+                    }
+                case 5:
+                    {
+                        String name1 = this.fileNameQueue.Dequeue();
+                        String name2 = this.fileNameQueue.Dequeue();
+                        String name3 = this.fileNameQueue.Dequeue();
+                        String name4 = this.fileNameQueue.Dequeue();
+                        img.Add(Image.FromFile(name1));
+                        img.Add(Image.FromFile(name2));
+                        img.Add(Image.FromFile(name3));
+                        img.Add(Image.FromFile(name4));
+                        Debug.WriteLine("Case: " + number);
+                        Debug.WriteLine("  Picture1 -> " + name1);
+                        Debug.WriteLine("  Picture2 -> " + name2);
+                        Debug.WriteLine("  Picture3 -> " + name3);
+                        Debug.WriteLine("  Picture4 -> " + name4);
+                        return new PicturePanelE1(this.Width, this.Height, img.ToArray());
+                    }
+                default:
+                    {
+                        String name1 = this.fileNameQueue.Dequeue();
+                        img.Add(Image.FromFile(name1));
+                        Debug.WriteLine("Case: " + number);
+                        Debug.WriteLine("  Picture1 -> " + name1);
+                        return new PicturePanelA1(this.Width, this.Height, img.ToArray());
+                    }
+            }
+        }
+
+        private void initFileNameList()
+        {
+            this.fileNameList.AddRange(System.IO.Directory.GetFiles(this.dir, "*", System.IO.SearchOption.AllDirectories));
+        }
+
+        private void initFileNameQueue()
+        {
+            List<String> list = new List<String>(this.fileNameList);
+            list = this.fileNameList.OrderBy(i => Guid.NewGuid()).ToList<String>();
+            this.fileNameQueue = new Queue<string>(list);
+        }
+    }
+
+}
